1e226c56a482a1a291ca71bed85677fe1e9af639
AniketKusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-07-19 17:36:42 +0530

    Update Program5.php

--- Program5.php
+++ Program5.php
@@ -3,29 +3,33 @@
 
 <?php
 
-// $a = "aniket";
-// var_dump($a);
+// 1  string data type
+ $a = "aniket";
+ var_dump($a);
+
+// 2   integer data type
+ $b =6;
+ var_dump($b);
+
+// 3 float data type
+ $c = 10.22;
+ var_dump($c);
+
+ // 4 boolean data type
+ $d = true;
+ var_dump($d);
 
 
-// $b =6;
-// var_dump($b);
+//  5  array data type 
+ $e = ["aniket" , "07"];
+var_dump($e) ;
+
+//  6 null data type
+ $f = null;
+ var_dump($f);
 
 
-// $c = 10.22;
-// var_dump($c);
-
-
-// $d = true;
-// var_dump($d);
-
-
-// $e = ["aniket" , "07"];
-// var_dump($e) ;
-
-
-// $f = null;
-// var_dump($f);
-
+//  7 object data type
 class bike {  
     function model() {  
          $model_name = "Royal Enfield";  
