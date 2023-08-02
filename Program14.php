0d09a36773b0838f89954fe88a151b9078f5fcaa
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-08-02 09:08:08 +0530

    Add files via upload

---
+++ Program14.php
@@ -0,0 +1,31 @@
+<!-- Write A Program In PHP To Demonstrate The Function Using Call By Value -->
+
+<?php
+
+    $a = 99;
+    $b = 100;
+
+    function swap ($x , $y)
+    {
+        $t = $x;
+        $x = $y;
+        $y = $t;
+
+        echo "Inside Function <br> ";
+        echo "a = ". $x . "<br>";
+        echo "b = " . $y . "<br>";
+    
+    }
+
+    echo "Outside The Function Before calling A Function <br>";
+    echo "a = " . $a . "<br>";
+    echo "b = " . $b . "<br>";
+
+    swap($a , $b);
+
+    echo " OutSide function After Calling Function <br>";
+    echo "a = " . $a . "<br>";
+    echo "b = " . $b . "<br>";
+
+
+?>
